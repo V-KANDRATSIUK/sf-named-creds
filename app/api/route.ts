c32656659c46c799c6a4d291d840c544a4d32d17
Viachaslau Kandratsiuk <150242659+ViacheslavKondratyuk@users.noreply.github.com>
2025-03-24 19:39:19 +0300

    added logs for test api

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,5 @@
 export async function GET(request: Request) {
     const requestHeaders = Object.fromEntries(request.headers.entries());
-    // console.log({r: requestHeaders})
+    console.log({r: requestHeaders})
     return Response.json({ data: 'hello world', requestHeaders });
 }
