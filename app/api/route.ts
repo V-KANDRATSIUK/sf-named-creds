b646895cbd497922137b9240b1bf2190d4b4f7ea
Viachaslau Kandratsiuk <150242659+ViacheslavKondratyuk@users.noreply.github.com>
2025-03-24 18:08:22 +0300

    corrected test api

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,5 @@
 export async function GET(request: Request) {
-    let requestHeaders = Object.fromEntries(request.headers.entries());
+    const requestHeaders = Object.fromEntries(request.headers.entries());
     // console.log({r: requestHeaders})
     return Response.json({ data: 'hello world', requestHeaders });
 }
