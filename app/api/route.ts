7ac830c75f327c609db9b4381d3b33a0259fc3c6
Viachaslau Kandratsiuk <150242659+ViacheslavKondratyuk@users.noreply.github.com>
2025-03-24 18:06:12 +0300

    created test api

---
+++ app/api/route.ts
@@ -0,0 +1,7 @@
+import { json } from "node:stream/consumers";
+
+export async function GET(request: Request) {
+    let requestHeaders = Object.fromEntries(request.headers.entries());
+    // console.log({r: requestHeaders})
+    return Response.json({ data: 'hello world', requestHeaders });
+}
